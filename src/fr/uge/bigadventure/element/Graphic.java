72d131d6de1d7effab150dc72a2997f7bb17b81d
Maëva L <maeva.laurent1@hotmail.com>
2023-12-07 21:29:06 +0100

    Create Graphic.java

---
+++ src/fr/uge/bigadventure/element/Graphic.java
@@ -0,0 +1,48 @@
+package fr.uge.bigadventure.element;
+
+import java.util.Objects;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+import fr.umlv.zen5.ScreenInfo;
+
+public class Graphic {
+  public static float windowWidth;
+  public static float windowHeight;
+	
+  public Graphic(ScreenInfo screenInfo) {
+	//il doit y avoir une fenetre
+	Objects.requireNonNull(screenInfo);
+	
+	//récup longueur et la largeur de la fenetre
+	Graphic.windowWidth = screenInfo.getWidth();
+    Graphic.windowHeight = screenInfo.getHeight();
+  }
+  
+  public static float getWidth() {
+	  //récupérer la largeur
+	  return windowWidth;
+  }
+  
+  public static float getHeight() {
+	  //récupérer la longueur
+	  return windowHeight;
+  }
+  
+  public void GraphicMap(Graphics2D map) {
+	  //on vérifie qu'on a bien une map à afficher
+	  Objects.requireNonNull(map);
+	  //fond noir
+	  map.setColor(Color.BLACK);
+	  /*
+	  map.fill(new Rectangle2D.Float(0, 0, width de la map, height de la map));
+	  for(var : listes de la grille){
+	  	for(var : elements de la liste){
+	  		si l'element n'a pas de skin -> on ne fait rien
+	  		sinon -> on affiche l'élement sur la map
+	  	}
+	  }
+	  */
+  }
+}
